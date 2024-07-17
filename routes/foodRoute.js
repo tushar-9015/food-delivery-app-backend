7dbba45bacac94f17839864e472d6f6467f132af
Tushar Joshi <tusharjoshi9015@gmail.com>
2024-07-17 12:38:02 +0530

    Set up Multer for image uploads and added upload route

--- routes/foodRoute.js
+++ routes/foodRoute.js
@@ -5,9 +5,17 @@ import multer from "multer"
 const foodRouter = express.Router()
 
 //Image Storage Engine
+const fileStorage = multer.diskStorage({
+    destination: "uploads",
+    filename:(req, file, cb) => {
+        return cb(null, `${Date.now()}${file.originalname}` )
+    }
+})
 
 
-foodRouter.post("/add", addFood)
+const upload = multer({storage: fileStorage})
+
+foodRouter.post("/add", upload.single("image"), addFood)
 
 
 
